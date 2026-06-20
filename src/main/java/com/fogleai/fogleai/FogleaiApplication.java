b9a64a3f741802cfe51725dc695e36037fe03c26
Addison Fogle <134630607+addison-fogle@users.noreply.github.com>
2026-06-20 14:37:22 -0600

    first commit

---
+++ src/main/java/com/fogleai/fogleai/FogleaiApplication.java
@@ -0,0 +1,13 @@
+package com.fogleai.fogleai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogleaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogleaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fogleai/fogleai/FogleaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fogleai.fogleai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogleaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
